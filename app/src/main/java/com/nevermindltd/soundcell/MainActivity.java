943efba4f16bc59fc74dfb4540470d5efb03db06
Lyreco <maisse.jeremy@sfr.fr>
2017-09-18 17:48:13 +0200

    small changes

--- app/src/main/java/com/nevermindltd/soundcell/MainActivity.java
+++ app/src/main/java/com/nevermindltd/soundcell/MainActivity.java
@@ -1,7 +1,12 @@
 package com.nevermindltd.soundcell;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView rightSpeakeersView;
     private ListView leftSpeakeersView;
     private ListView cellsView;
+    private ImageView addSpeaker;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         rightSpeakeersView = (ListView) findViewById(R.id.LayoutRightSpeakers);
         leftSpeakeersView = (ListView) findViewById(R.id.LayoutLeftSpeakers);
-        cellsView = (ListView) findViewById(R.id.LayoutCells);
+        addSpeaker = (ImageView) findViewById(R.id.addSpeaker);
+        //cellsView = (ListView) findViewById(R.id.LayoutCells);
 
         List<Speaker> speakers = getSpeakers();
         List<Cell> cells = getCells();
@@ -35,15 +42,26 @@ public class MainActivity extends AppCompatActivity {
 
         leftSpeakeersView.setAdapter( new SpeakerAdapter(speakersLEFT,getApplicationContext()));
         rightSpeakeersView.setAdapter( new SpeakerAdapter(speakersRIGHT,getApplicationContext()));
-        cellsView.setAdapter( new CellAdapter(cells,getApplicationContext()));
+        addSpeaker.setOnClickListener(clicAddSpeackerListerner);
+        //cellsView.setAdapter( new CellAdapter(cells,getApplicationContext()));
 
     }
+
+    private OnClickListener clicAddSpeackerListerner= new OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this,"ADD SPEACKER",Toast.LENGTH_LONG);
+        }
+    };
+
     private List<Speaker> getSpeakers(){
         List<Speaker> speakers = new ArrayList<Speaker>();
         speakers.add(new Speaker("premier",Position.left));
         speakers.add(new Speaker("second",Position.right));
         speakers.add(new Speaker("troisième",Position.left));
         speakers.add(new Speaker("quatrième",Position.right));
+        speakers.add(new Speaker("cinquième",Position.left));
+        speakers.add(new Speaker("sixième",Position.right));
         return speakers;
     }
     private List<Cell> getCells(){
